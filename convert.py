568dfbc79a65ddb98b150d245a55123019ea9c7c
Zach Bolin <zdbolin@gmail.com>
2022-10-23 17:59:04 -0500

    dev

--- convert.py
+++ convert.py
@@ -4,34 +4,31 @@ import datetime
 SYMPH = "bb.csv"
 PV = "bb3.csv"
 
+SYMPH = "HFEA.csv"
+PV = "HFEA2.csv"
 
 def main():
     with open(SYMPH, mode='r', encoding='utf-8-sig') as file:
         csvFile = csv.reader(file)
-        # print("Start Date, Assets, Weights")
-        write("Start Date,Assets,Weights")
         assets = ""
         for line in csvFile:
             s = ', '.join(line)
             trade = ""
             if "Date" in s:
-                #    print(s)
-                assets += s[18:len(s)].replace("$USD", "VOO")
-                print(assets)
-#            if 1 == 2:
+                assets += "Date,"
+                assets += s[18:len(s)]
+                write(assets)
             if "Yes" in s:
                 date = s[0:10]
-                d = datetime.datetime.strptime(date, '%Y-%m-%d')
-                #        print(datetime.date.strftime(d, "%m/%d/%y"))
                 trade += date
-                trade += ',"'
-                trade += assets
-                trade += '","'
-#                print(s)
-#                print(s[17:len(s)])
-                trade += s[17:len(s)].replace("-", "0%")
-
-                trade += '"'
+                trade += ','
+                trade += s[17:len(s)].replace("-", "").replace(" ", "")
+                write(trade)
+            if "No" in s:
+                date = s[0:10]
+                trade += date
+                trade += ','
+                trade += s[16:len(s)].replace("-", "").replace(" ","")
                 write(trade)
 
 
