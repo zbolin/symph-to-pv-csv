3b8b5ee64d2765458e0d0455d0461bad7e9c6d1a
Zach Bolin <zdbolin@gmail.com>
2022-10-23 19:25:36 -0500

    making changes

--- convert.py
+++ convert.py
@@ -1,10 +1,12 @@
 import csv
 import datetime
-
+import os
 
 def main(fileName):
     outputFile = fileName.replace(".csv", "")
     outputFile += "_pv.csv"
+    if os.path.exists(outputFile):
+        os.remove(outputFile)
     with open(fileName, mode='r', encoding='utf-8-sig') as file:
         csvFile = csv.reader(file)
         assets = ""
@@ -14,9 +16,9 @@ def main(fileName):
             if "Date" in line:
                 usdIndex = line.index("$USD")
                 line.remove("$USD")
-                s = ', '.join(line).replace(" ","")
+                s = ', '.join(line).replace(" ", "")
                 assets += "Date,"
-                assets += s[18:len(s)]
+                assets += s[15:len(s)]
                 write(assets, outputFile)
             if "Yes" in line:
                 del line[usdIndex]
@@ -35,7 +37,7 @@ def main(fileName):
                 trade += s[16:len(s)].replace("-", "").replace(" ","")
                 write(trade, outputFile)
 
-    print("Finished converting to :", outputFile)
+    print("SUCCESS! New file created :", outputFile)
 
 def write(line, outputFile):
     file1 = open(outputFile, "a")  # append mode
@@ -45,6 +47,7 @@ def write(line, outputFile):
 
 
 if __name__ == "__main__":
-    file = input("Enter name of Composer file to convter to Portfolio Visualizer: ")
+    file = input("Enter name of file to convert: ")
+    
     main(file)
     #main("HFEA.csv")
